e28709b6aa5e9efcbde57a0eb607c3120a51802f
Monir Hoshen <getmonirr@gmail.com>
2023-04-04 19:51:28 +0600

    index route done

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
 
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      {
+        index: true,
+        element: <Index />,
+      },
       {
         path: "contacts/:contactId",
         element: <Contact />,
